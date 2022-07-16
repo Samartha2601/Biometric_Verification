6395ebd75b667afb5651f0f62ff98d15fa4cf606
Samartha2601 <62658546+Samartha2601@users.noreply.github.com>
2022-07-16 18:24:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,21 +2,27 @@ from flask import Flask,render_template
 import numpy as np
 from flask import jsonify,request
 import joblib
-from Scripts.mongodb import *
+# from Scripts.mongodb import *
 import json
 app=Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def landing():
+    return render_template('landing.html')
+
 @app.route('/login')
 def login():
-
     return render_template('login.html')
 
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
 if __name__=='__main__':
     app.run(port=5000,debug=True)
